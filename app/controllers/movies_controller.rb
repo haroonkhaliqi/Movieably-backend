6dcea03e0170e7dcfd6a08fb06df068fcf0d42a8
Haroon Khaliqi <haroonkhaliqi12@Haroons-MBP.attlocal.net>
2023-07-10 12:42:30 -0700

    added create action

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -3,4 +3,24 @@ class MoviesController < ApplicationController
     @movies = Movie.all
     render :index
   end
+
+  def create
+    @movie = Movie.create(
+      title: params[:title],
+      year: params[:year],
+      rated: params[:rated],
+      released: params[:released],
+      runtime: params[:runtime],
+      genre: params[:genre],
+      director: params[:director],
+      writer: params[:writer],
+      actors: params[:actors],
+      plot: params[:plot],
+      language: params[:language],
+      country: params[:country],
+      awards: params[:awards],
+      poster: params[:poster],
+    )
+    render :show
+  end
 end
